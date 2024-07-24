8707e5c8a64140c0c3b657a0c8199fbfbcf01e06
ehabmohamed6699 <ihabmohamed6699@gmail.com>
2024-07-24 17:10:16 +0300

    fixed some layout issues

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import UseState from './components/UseState/UseState'
 
 function App() {
   return (
-    <>
+    <div style={{display: 'flex', flexDirection:"column", alignItems:"center", overflowX:"hidden", width:"100%"}}>
       <h1>React Hooks Examples</h1>
       <img src="https://raw.githubusercontent.com/alDuncanson/react-hooks-snippets/master/icon.png" alt="React Hooks" width={300}/>
       <UseState/>
@@ -19,7 +19,7 @@ function App() {
       <UseContext/>
       <h4>Source code: <a href='https://github.com/ehabmohamed6699/react-hooks'>https://github.com/ehabmohamed6699/react-hooks</a></h4>
       <h4>Reference: <a href='https://www.youtube.com/@PedroTechnologies'>https://www.youtube.com/@PedroTechnologies</a></h4>
-    </>
+    </div>
   )
 }
 
